891c83bef3d3cb2041a5c75c269db1b8021fdd17
Jari Hämäläinen <nuumiofi@gmail.com>
2024-10-09 20:28:02 +0300

    feat: add dummy a package

---
+++ packages/fl-dummy-a/README.md
@@ -0,0 +1,3 @@
+# `fl_dummy_a`
+
+This is just a dummy flutter lib to test monorepo things.
---
+++ packages/fl-dummy-a/lib/fl_dummy_a.dart
@@ -0,0 +1,13 @@
+library fl_dummy_a;
+
+import 'package:flutter/widgets.dart';
+
+/// A dummy A widget.
+class DummyAWidget extends StatelessWidget {
+  const DummyAWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Dummy A');
+  }
+}
